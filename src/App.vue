4c852f8e344aa36a7677cd7abec889a84e5c7810
EC2 Default User <ec2-user@ip-172-31-20-78.ca-central-1.compute.internal>
2020-08-25 05:07:52 +0000

    update auth

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <amplify-authenticator>
   <div id="app">
+    <div v-if="!signedIn">
+     <amplify-authenticator></amplify-authenticator>
+      </div>
+      <div v-if="signedIn">
+        <amplify-sign-out class="signout"></amplify-sign-out>
     <div class="text-gray-800 md:text-lg italic font-light">Which fundraiser proposal do you like best?</div>
     <div class="mx-auto flex mt-4">
       <button v-for="(proposal, index) in proposals" v-bind:key="proposal.id" @click="castVote(proposal.id)"
         v-bind:class="`focus:outline-none flex-1 h-max text-white text-sm sm:text-lg font-semibold py-2 px-2 mx-1 md:h-18 h-18 rounded bg-${proposalColors[index]}-600 hover:bg-${proposalColors[index]}-800`">
         {{proposal.name}}<p>{{proposal.votes}}</p>
       </button>
+      </div>
     </div>
     <h1 class="text-gray-800 font-bold mt-8 mb-3">Live updates</h1>
     <VoteChart v-bind:chartData="chartData" width="200" height="110"></VoteChart>
   </div>
-  <amplify-sign-out></amplify-sign-out>
-  </amplify-authenticator>
+
 </template>
 
 <script>
@@ -21,6 +25,8 @@ import { API, graphqlOperation } from "aws-amplify";
 import * as queries from "./graphql/queries";
 import * as mutations from "./graphql/mutations";
 import * as subscriptions from "./graphql/subscriptions";
+import { AmplifyEventBus } from 'aws-amplify-vue'
+import { Auth } from 'aws-amplify'
 
 export default {
   components: { VoteChart },
@@ -30,6 +36,23 @@ export default {
       proposalColors: ["red", "pink", "purple", "indigo"]
     };
   },
+  //---------------------------------- auth ---------------------
+    async beforeCreate() {
+    try {
+      const user = await Auth.currentAuthenticatedUser()
+      this.signedIn = true
+    } catch (err) {
+      this.signedIn = false
+    }
+    AmplifyEventBus.$on('authState', info => {
+      if (info === 'signedIn') {
+        this.signedIn = true
+      } else {
+        this.signedIn = false
+      }
+    });
+  },
+  //---------------------------------- auth ---------------------
   computed: {
     chartData: function() {
       return {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Amplify from 'aws-amplify'
 import awsconfig from './aws-exports'
+import '@aws-amplify/ui-vue';
 Amplify.configure(awsconfig)
 
 Vue.config.productionTip = false
